9a367803c4d133d6ea58c7879415ba5c5ff63aea
jose <joseluis19963@gmail.com>
2020-10-11 11:19:22 -0500

    MVC.

---
+++ controllers/user.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var controller = {
+    probando: function(req, res){
+        return res.status(200).send({
+            message: "Soy el metodo probando"
+        });
+    },
+    testeando: function(req, res){
+        return res.status(200).send({
+            message: "Soy el metodo testeando"
+        });
+    }
+
+};
+
+module.exports = controller;
